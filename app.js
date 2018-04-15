f57f00e84b26687040d87a71f4505234bf215adf
Keith Lashley <lashleykeith@gmail.com>
2018-04-15 19:08:58 +0900

    3 Create
    New post

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var Blog = mongoose.model("Blog", blogSchema);
 app.get("/", function(req, res){
 	res.redirect("/blogs");
 });
-
+// INDEX ROUTE
 app.get("/blogs", function(req, res){
 	Blog.find({}, function(err, blogs){
 		if(err){
@@ -35,6 +35,24 @@ app.get("/blogs", function(req, res){
 	});
 });
 
+// NEW ROUTE
+app.get("/blogs/new", function(req, res){
+	res.render("new");
+});
+// CREATE ROUTE
+app.post("/blogs", function(req, res){
+	// create blog
+	Blog.create(req.body.blog, function(err, newBlog){
+		if(err){
+			res.render("new");
+		} else{
+			// then, redirect to the index
+			res.redirect("/blogs");
+		}
+	});
+});
+
 app.listen(4900, function(){
 	console.log("SERVER IS RUNNING");
 });
+
